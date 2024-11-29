fc71853a341942ba83450d5776da678f90533ed5
Lucas Leiradella <107800073+leiradella@users.noreply.github.com>
2024-11-29 10:04:24 +0000

    added the rest of the libraries
    GLFW, GLEW, mgl

--- CGJ_Project/main.cpp
+++ CGJ_Project/main.cpp
@@ -2,6 +2,9 @@
 #include <glm/vec4.hpp> // glm::vec4
 #include <glm/mat4x4.hpp> // glm::mat4
 #include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include "../mgl/mgl.hpp"
+
+
 glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
 {
     glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
