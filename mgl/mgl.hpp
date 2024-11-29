a64384eac5acd16b5f2d8b26352cc17cfcaacf99
Lucas Leiradella <107800073+leiradella@users.noreply.github.com>
2024-11-29 14:20:16 +0000

    start of A2

--- mgl/mgl.hpp
+++ mgl/mgl.hpp
@@ -13,6 +13,7 @@
 #include <GLFW/glfw3.h>
 
 #include "./mglApp.hpp"          // IWYU pragma: keep
+#include "./mglCamera.hpp"       // IWYU pragma: keep
 #include "./mglConventions.hpp"  // IWYU pragma: keep
 #include "./mglError.hpp"        // IWYU pragma: keep
 #include "./mglShader.hpp"       // IWYU pragma: keep
