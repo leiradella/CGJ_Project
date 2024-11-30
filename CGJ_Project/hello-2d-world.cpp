f273168496202033632c1b301dce2b4f7db7c7a7
FlipAllTheTables <rafaelbgwr@gmail.com>
2024-11-30 21:49:46 +0000

    Added additional objects to scene
    - All objects are colored
    - All objects are correctly rotated

--- CGJ_Project/hello-2d-world.cpp
+++ CGJ_Project/hello-2d-world.cpp
@@ -28,7 +28,7 @@ class MyApp : public mgl::App {
 
  private:
   const GLuint POSITION = 0, COLOR = 1;
-  GLuint VaoId[3], VboId[2];
+  GLuint VaoId[7], VboId[2];
   std::unique_ptr<mgl::ShaderProgram> Shaders;
   GLint MatrixId;
 
@@ -87,10 +87,14 @@ const Vertex parallelogramVertex[] = {
 const GLubyte parallelogramIndices[] = { 0, 1, 2, 0, 2, 3};
 
 void MyApp::createBufferObjects() {
-  glGenVertexArrays(3, VaoId);
-  addBufferTriangle(0, 1.0f, 1.0f, 0.0f, 1.0f);
-  addBufferSquare(1, 0.0f, 1.0f, 1.0f, 1.0f);
-  addBufferParallelogram(2, 1.0f, 0.0f, 1.0f, 1.0f);
+  glGenVertexArrays(7, VaoId);
+  addBufferTriangle(0, 0.9254901961f, 0.1098039216f, 0.1411764706f, 1.0f);
+  addBufferTriangle(1, 0.2745098039f, 0.5882352941f, 0.9294117647f, 1.0f);
+  addBufferTriangle(2, 0.4431372549f, 0.7490196078f, 0.2705882353f, 1.0f);
+  addBufferTriangle(3, 0.8274509804f, 0.831372549f, 0.8392156863f, 1.0f);
+  addBufferTriangle(4, 0.9568627451f, 0.5137254902f, 0.1215686275f, 1.0f);
+  addBufferSquare(5, 1.0f, 0.7607843137f, 0.05882352941f, 1.0f);
+  addBufferParallelogram(6, 0.6862745098f, 0.5960784314f, 0.8274509804f, 1.0f);
   glBindVertexArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -185,7 +189,7 @@ void MyApp::addBufferParallelogram(int i, float r, float g, float b, float a) {
 }
 
 void MyApp::destroyBufferObjects() {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 6; i++) {
         glBindVertexArray(VaoId[i]);
         glDisableVertexAttribArray(POSITION);
         glDisableVertexAttribArray(COLOR);
@@ -196,9 +200,17 @@ void MyApp::destroyBufferObjects() {
 
 ////////////////////////////////////////////////////////////////////////// SCENE
 
-const glm::mat4 I(1.0f);
-const glm::mat4 parallelogram = glm::rotate(glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-const glm::mat4 triangle = glm::translate(glm::vec3(-0.75f, -0.75f, 0.0f));
+const glm::mat4 m1 = glm::translate(glm::vec3(0.5f, 0.5f, 0.0f))
+                    * glm::rotate(glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+const glm::mat4 m2 = glm::translate(glm::vec3(0.5f, -0.5f, 0.0f))
+                    * glm::rotate(glm::radians(-135.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+const glm::mat4 m3 = glm::rotate(glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));    
+const glm::mat4 m4 = glm::rotate(glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+const glm::mat4 m5 = glm::translate(glm::vec3(-1.0f, 0.0f, 0.0f))
+                    * glm::rotate(glm::radians(-45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+const glm::mat4 m6 = glm::translate(glm::vec3(-1.0f, -1.0f, 0.0f));
+const glm::mat4 m7 = glm::translate(glm::vec3(-0.5f, 0.0f, 0.0f))
+                    * glm::rotate(glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
 void MyApp::drawScene() {
   // Drawing directly in clip space
@@ -206,19 +218,39 @@ void MyApp::drawScene() {
   glBindVertexArray(VaoId[0]);
   Shaders->bind();
 
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(triangle));
+  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(m1));
   glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
                  reinterpret_cast<GLvoid *>(0));
 
   glBindVertexArray(VaoId[1]);
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(I));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE,
+  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(m2));
+  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
       reinterpret_cast<GLvoid*>(0));
 
   glBindVertexArray(VaoId[2]);
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(parallelogram));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE,
+  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(m3));
+  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
 	  reinterpret_cast<GLvoid*>(0));
+
+  glBindVertexArray(VaoId[3]);
+  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(m4));
+  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
+      reinterpret_cast<GLvoid*>(0));
+
+  glBindVertexArray(VaoId[4]);
+  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(m5));
+  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
+      reinterpret_cast<GLvoid*>(0));
+
+  glBindVertexArray(VaoId[5]);
+  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(m6));
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE,
+      reinterpret_cast<GLvoid*>(0));
+
+  glBindVertexArray(VaoId[6]);
+  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(m7));
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE,
+      reinterpret_cast<GLvoid*>(0));
 
   Shaders->unbind();
   glBindVertexArray(0);
