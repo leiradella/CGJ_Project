350670a9094b8911b735bae4afea7a1de6520bc3
FlipAllTheTables <rafaelbgwr@gmail.com>
2024-12-08 12:44:16 +0000

    Added more methods and variables to SceneNode class

--- CGJ_Project/scene-graph.cpp
+++ CGJ_Project/scene-graph.cpp
@@ -1,1 +1,20 @@
 #include "scene-graph.hpp"
+
+SceneNode::SceneNode(mgl::Mesh* mesh, glm::vec4 color, mgl::ShaderProgram* Shaders) {
+	this->mesh = mesh;
+	this->color = color;
+	parent = NULL;
+	ModelMatrix = glm::mat4(1.0f);
+	this->Shaders = Shaders;
+}
+
+void SceneNode::draw() {
+	if (mesh) {
+		mesh->draw();
+	}
+}
+
+void SceneNode::addChild(SceneNode* child) {
+	children.push_back(*child);
+	child->parent = this;
+}
--- CGJ_Project/scene-graph.hpp
+++ CGJ_Project/scene-graph.hpp
@@ -3,10 +3,16 @@
 #include "../mgl/mgl.hpp"
 
 class SceneNode {
+public:
+	SceneNode(mgl::Mesh* m, glm::vec4 color, mgl::ShaderProgram* Shaders);
+	void addChild(SceneNode* child);
+	void draw();
+
 protected:
 	SceneNode* parent;
 	std::vector<SceneNode> children;
 	mgl::Mesh* mesh;
+	glm::vec4 color;
 	glm::mat4 ModelMatrix;
 	mgl::ShaderProgram* Shaders;
 };
