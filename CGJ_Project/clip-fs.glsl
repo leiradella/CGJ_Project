560c70c26478f58680bf11c3367bf2c743247b70
FlipAllTheTables <rafaelbgwr@gmail.com>
2024-12-12 11:25:36 +0000

    Removed unnecessary shader and object files

--- CGJ_Project/clip-fs.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 outColor;
-
-uniform vec4 Color;
-
-void main(void) {
-    outColor = Color;
-}
--- CGJ_Project/clip-vs.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-in vec4 inPosition;
-
-uniform mat4 Matrix;
-
-void main(void) {
-    gl_Position = Matrix * inPosition;
-}
