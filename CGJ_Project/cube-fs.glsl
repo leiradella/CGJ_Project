a907cef44e3fa653445d01cafa926cf2303bfa13
FlipAllTheTables <rafaelbgwr@gmail.com>
2024-12-12 11:44:56 +0000

    Removed unnecessary code from cube-fs.glsl sfragment shader

--- CGJ_Project/cube-fs.glsl
+++ CGJ_Project/cube-fs.glsl
@@ -8,36 +8,11 @@ out vec4 FragmentColor;
 
 uniform vec4 BaseColor;
 
-vec3 constantColor(void) {
-    return vec3(0.5);
-}
-
-vec3 positionColor(void) {
-    return (exPosition + vec3(1.0)) * 0.5;
-}
-
-vec3 uvColor(void) {
-    return vec3(exTexcoord, 0.0);
-}
-
 vec3 normalColor(void) {
     return (exNormal + vec3(1.0)) * 0.5;
 }
 
-vec3 diffuseColor(void) {
-    vec3 N = normalize(exNormal);
-    vec3 direction = vec3(1.0, 0.5, 0.25);
-    float intensity = max(dot(direction, N), 0.0);
-    return vec3(intensity);
-}
-
 void main(void)
 {
-    vec3 color;
-    // color = constantColor();
-    // color = positionColor();
-    // color = uvColor();
-    color = normalColor();
-    // color = diffuseColor();
-    FragmentColor = BaseColor + 0.2 * vec4(color, 1.0);
+    FragmentColor = BaseColor + 0.2 * vec4(normalColor(), 1.0);
 }
